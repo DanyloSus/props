c299f22c53a297db778d94d9267858e57fc429a4
DanyloSus <danil.sushko19@gmail.com>
2022-12-15 16:53:54 +0200

    something

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export default function App () {
+  const date = new Date ();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12 && hours >= 6) {
+    timeOfDay = "morning"
+  }
+  else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  }
+  else if (hours >= 17 && hours < 22) {
+    timeOfDay = "evening"
+  }
+  else {
+    timeOfDay = "night"
+  }
+  
   return (
     <div>
-      <h1>Check!</h1>
+      <h1>Good {timeOfDay}</h1>
     </div>
   );
 }
