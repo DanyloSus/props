ebc3e478884fad6f1bc2faa63c0735c09e1ad48b
DanyloSus <danil.sushko19@gmail.com>
2022-12-15 20:09:02 +0200

    did site with kitties >:)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import Contact from './components/Contact';
 export default function App() {
   return (
       <div className="contacts">
-        <h1>C</h1>
+        <Contact 
+          img="mr-whiskerson.png"
+          name="Mr. Whiskerson"
+          number="(212) 555-1234"
+          mail="mr.whiskaz@catnap.meow"
+        />
+        <Contact 
+          img="fluffykins.png"
+          name="Fluffykins"
+          number="(212) 555-2345"
+          mail="fluff@me.com"
+        />
+        <Contact 
+          img="felix.png"
+          name="Felix"
+          number="(212) 555-4567"
+          mail="thecat@hotmail.com"
+        />
+        <Contact 
+          img="pumpkin.png"
+          name="Pumpkin"
+          number="(0800) CAT KING"
+          mail="pumpkin@scrimba.com"
+        />
       </div>
   )
 }
