f94facd3833ec7398cf2c7ccda4509d7a194f1ff
DanyloSus <danil.sushko19@gmail.com>
2022-12-15 19:44:23 +0200

    something

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
+import Contact from './components/Contact';
 
-export default function App () {
-  const date = new Date ();
-  const hours = date.getHours();
-  let timeOfDay;
-
-  if (hours < 12 && hours >= 6) {
-    timeOfDay = "morning"
-  }
-  else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon"
-  }
-  else if (hours >= 17 && hours < 22) {
-    timeOfDay = "evening"
-  }
-  else {
-    timeOfDay = "night"
-  }
-  
+export default function App() {
   return (
-    <div>
-      <h1>Good {timeOfDay}</h1>
-    </div>
-  );
+      <div className="contacts">
+        <h1>C</h1>
+      </div>
+  )
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact () {
+    return (
+        <div className="contact-card">
+              <h1>C</h1>
+          </div>
+    );
+}
